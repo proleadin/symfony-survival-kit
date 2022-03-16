5e6b7349e0003408169ef553b3b30fa537dff696
Piotr Bernad <pbernad.leadin@gmail.com>
2022-03-16 11:08:00 +0100

    logger_handlers : add gelf handler

--- src/Logging/Handler/StreamHandler.php
+++ src/Logging/Handler/StreamHandler.php
@@ -8,8 +8,7 @@ use Monolog\Handler\StreamHandler as MonologStreamHandler;
 
 class StreamHandler extends MonologStreamHandler
 {
-    private string $sConfigPath;
-    private ?array $aConfig = null;
+    use HandlerTrait;
 
     public function __construct(string $sStream, string $sLevel, string $sConfigPath)
     {
@@ -23,33 +22,7 @@ class StreamHandler extends MonologStreamHandler
      */
     public function handle(array $aRecord): bool
     {
-file_put_contents('/code/pbernad/pbernad.log', PHP_EOL . 'handle_' . $aRecord['message'] . PHP_EOL, FILE_APPEND);
-
-        $bIsHandlingByDefault = parent::isHandling($aRecord);
-        if ($bIsHandlingByDefault) {
-            return parent::handle($aRecord);
-        }
-
-        if ($aRecord['level'] !== Logger::DEBUG) {
-file_put_contents('/code/pbernad/pbernad.log', PHP_EOL . '24' . PHP_EOL, FILE_APPEND);
-            return false;
-        } else if (!isset($aRecord['context']['context'])) {
-file_put_contents('/code/pbernad/pbernad.log', PHP_EOL . '27' . PHP_EOL, FILE_APPEND);
-            return false;
-        }
-
-        $this->loadConfig();
-
-        $sContext = $aRecord['context']['context'];
-        if (!isset($this->aConfig[$sContext])) {
-file_put_contents('/code/pbernad/pbernad.log', PHP_EOL . '35' . PHP_EOL, FILE_APPEND);
-            return false;
-        } else if (\time() > $this->aConfig[$sContext]) {
-file_put_contents('/code/pbernad/pbernad.log', PHP_EOL . '38' . PHP_EOL, FILE_APPEND);
-            return false;
-        }
-
-        return parent::handle($aRecord);
+        return $this->handleLog($aRecord);
     }
 
     /**
@@ -57,22 +30,6 @@ file_put_contents('/code/pbernad/pbernad.log', PHP_EOL . '38' . PHP_EOL, FILE_AP
      */
     public function isHandling(array $aRecord): bool
     {
-file_put_contents('/code/pbernad/pbernad.log', PHP_EOL . 'isHandling' . PHP_EOL, FILE_APPEND);
-
         return true;
-    }
-
-    private function loadConfig()
-    {
-file_put_contents('/code/pbernad/pbernad.log', PHP_EOL . 'loadConfig' . PHP_EOL, FILE_APPEND);
-
-        if (!is_null($this->aConfig)) {
-            return;
-        }
-
-        $sConfigJson = @\file_get_contents($this->sConfigPath) ? : "";
-        $this->aConfig = \json_decode($sConfigJson, true) ? : [];
-
-file_put_contents('/code/pbernad/pbernad.log', PHP_EOL . 'config_loaded' . PHP_EOL, FILE_APPEND);
     }
 }

b5bb050f1ca91f7bcfd3b447eadeec54eb75ba58
Piotr Bernad <pbernad.leadin@gmail.com>
2022-05-08 20:35:50 +0200

    maintenance_mode : add query param, template

--- EventSubscriber/MaintenaceModeSubscriber.php
+++ EventSubscriber/MaintenaceModeSubscriber.php
@@ -5,40 +5,54 @@ namespace Leadin\SurvivalKitBundle\EventSubscriber;
 use Leadin\SurvivalKitBundle\Logging\LogContext;
 use Leadin\SurvivalKitBundle\Logging\Logger;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Environment;
 
 /*
  * Checks if maintenance mode is enabled and prevents access to the site if needed
  */
 class MaintenaceModeSubscriber implements EventSubscriberInterface
 {
-    private bool $bMaintenanceMode = false;
+    private const BUNDLE_ASSETS_URI = 'survivalkit/assets';
 
-    public function __construct(ParameterBagInterface $parameterBag)
+    private bool $bMaintenanceMode = false;
+    private Environment $twig;
+
+    public function __construct(ParameterBagInterface $parameterBag, Environment $twig)
     {
         $this->bMaintenanceMode = $parameterBag->get('survival_kit.maintenance_mode');
+        $this->twig = $twig;
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            KernelEvents::REQUEST => 'onKernelRequest'
+            KernelEvents::REQUEST => ['onKernelRequest', 256]
         ];
     }
 
     public function onKernelRequest(RequestEvent $event): void
     {
-        if (!$event->isMasterRequest() || !$this->bMaintenanceMode) {
+        if (!$this->bMaintenanceMode || !$event->isMasterRequest() || $this->isBundleAssetsRequest($event)) {
             return;
         }
 
-        // TODO check for query param
+        $sQueryMaintenanceMode = $event->getRequest()->query->get('maintenance_mode');
+        if (!is_null($sQueryMaintenanceMode) && !(bool)$sQueryMaintenanceMode) {
+            return;
+        }
 
-        // $request = $requestEvent->getRequest();
-        $content = "MAINTENACE MODE";
-        $event->setResponse(new Response($content, 503));
+        $sContent = $this->twig->render('@SurvivalKit/maintenance_mode/works_in_progress.twig');
+        $event->setResponse(new Response($sContent, Response::HTTP_SERVICE_UNAVAILABLE));
         $event->stopPropagation();
+    }
+
+    private function isBundleAssetsRequest(RequestEvent $event): bool
+    {
+        return \strpos($event->getRequest()->getRequestUri(), self::BUNDLE_ASSETS_URI) !== false;
     }
 }

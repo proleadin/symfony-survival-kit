af2a5adc9a2618dfd61a20dcc599efe8674e7156
Piotr Bernad <pbernad.leadin@gmail.com>
2022-05-08 21:56:15 +0200

    maintenance_mode : add logs

--- EventSubscriber/MaintenanceModeSubscriber.php
+++ EventSubscriber/MaintenanceModeSubscriber.php
@@ -15,7 +15,7 @@ use Twig\Environment;
 /*
  * Checks if maintenance mode is enabled and prevents access to the site if needed
  */
-class MaintenaceModeSubscriber implements EventSubscriberInterface
+class MaintenanceModeSubscriber implements EventSubscriberInterface
 {
     private const BUNDLE_ASSETS_URI = 'survivalkit/assets';
 
@@ -37,16 +37,26 @@ class MaintenaceModeSubscriber implements EventSubscriberInterface
 
     public function onKernelRequest(RequestEvent $event): void
     {
+        Logger::debug("[MaintenaceModeSubscriber] Checking is maintenance mode enabled", LogContext::SSK_BUNDLE());
+
         if (!$this->bMaintenanceMode || !$event->isMasterRequest() || $this->isBundleAssetsRequest($event)) {
+            Logger::debug("[MaintenaceModeSubscriber] Maintenance mode disabled", LogContext::SSK_BUNDLE());
+
             return;
         }
 
         $sQueryMaintenanceMode = $event->getRequest()->query->get('maintenance_mode');
         if (!is_null($sQueryMaintenanceMode) && !(bool)$sQueryMaintenanceMode) {
+            Logger::debug("[MaintenaceModeSubscriber] Maintenance mode disabled by query param", LogContext::SSK_BUNDLE());
+
             return;
         }
 
-        $sContent = $this->twig->render('@SurvivalKit/maintenance_mode/works_in_progress.twig');
+        Logger::warning("[MaintenaceModeSubscriber] Maintenance mode enabled. Accessing site not possible!", LogContext::SSK_BUNDLE());
+
+        // TODO documentation
+
+        $sContent = $this->twig->render('@SurvivalKit/maintenance_mode.twig');
         $event->setResponse(new Response($sContent, Response::HTTP_SERVICE_UNAVAILABLE));
         $event->stopPropagation();
     }

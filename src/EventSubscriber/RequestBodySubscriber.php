9d0efedf08b1b07d43186fb0484bff7e3e11628a
Piotr Bernad <pbernad.leadin@gmail.com>
2023-03-06 10:55:53 +0100

    logger_request_subscriber : improve RequestBodySubscriber error handling

--- src/EventSubscriber/RequestBodySubscriber.php
+++ src/EventSubscriber/RequestBodySubscriber.php
@@ -1,10 +1,14 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Leadin\SurvivalKitBundle\EventSubscriber;
 
+use Leadin\SurvivalKitBundle\Logging\Logger;
+use Leadin\SurvivalKitBundle\Logging\LogContext;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
@@ -31,8 +35,19 @@ class RequestBodySubscriber implements EventSubscriberInterface
 
         $data = \json_decode($request->getContent(), true);
         if (\json_last_error() !== JSON_ERROR_NONE) {
-            throw new BadRequestHttpException('Invalid json body: ' . json_last_error_msg());
+            $sError = \json_last_error_msg();
+            Logger::info("Invalid json body: $sError", LogContext::SSK_BUNDLE(), [
+                "requestContent" => $request->getContent()
+            ]);
+
+            $response = new JsonResponse([
+                "message" => "Invalid request payload",
+                "error" => $sError
+            ], JsonResponse::HTTP_BAD_REQUEST);
+
+            $response->send();
+        } else {
+            $request->request->replace(\is_array($data) ? $data : []);
         }
-        $request->request->replace(\is_array($data) ? $data : []);
     }
 }

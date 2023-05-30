32da957c48dba262bc2e31584d380eba4f9338bc
Piotr Bernad <pbernad.leadin@gmail.com>
2023-05-30 09:40:27 +0200

    bug-4486955766 : fix container issue after deployment composer install

--- src/Controller/DeploymentController.php
+++ src/Controller/DeploymentController.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Leadin\SurvivalKitBundle\Controller;
 
@@ -9,9 +11,7 @@ use Leadin\SurvivalKitBundle\Event\SurvivalKitEvents;
 use Leadin\SurvivalKitBundle\HttpHelper\HttpServerHelper\ITokenAuthenticatedController;
 use Leadin\SurvivalKitBundle\Logging\Logger;
 use Leadin\SurvivalKitBundle\Logging\LogContext;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -39,41 +39,47 @@ class DeploymentController extends AbstractController implements ITokenAuthentic
      */
     public function githubWebhook(Request $request): JsonResponse
     {
-        Logger::debug("[DeploymentController] Github webhook deployment request received", LogContext::DEPLOYMENT(), ['payload' => $request->getContent()]);
+        Logger::debug("Github webhook deployment request received", LogContext::DEPLOYMENT(), ['payload' => $request->getContent()]);
 
         $sAction = $request->request->get('action');
         $aPullRequestData = $request->request->get('pull_request');
         if (!$sAction) {
-            Logger::error("[DeploymentController] Github webhook bad request : field 'action' must not be blank", LogContext::DEPLOYMENT());
+            Logger::error("Github webhook bad request : field 'action' must not be blank", LogContext::DEPLOYMENT());
 
             return $this->json(["message" => "field 'action' must not be blank"], Response::HTTP_BAD_REQUEST);
         } elseif (!$aPullRequestData || !is_array($aPullRequestData)) {
-            Logger::error("[DeploymentController] Github webhook bad request : field 'pull_request' must not be blank", LogContext::DEPLOYMENT());
+            Logger::error("Github webhook bad request : field 'pull_request' must not be blank", LogContext::DEPLOYMENT());
 
             return $this->json(["message" => "field 'pull_request' must not be blank"], Response::HTTP_BAD_REQUEST);
         }
 
         $pullRequest = new PullRequest($sAction, $aPullRequestData);
+        if (!$pullRequest->isMerged()) {
+            Logger::debug("PullRequest not merged, deployment not proceed", LogContext::DEPLOYMENT());
+
+            return $this->json(["message" => "PullRequest not merged, deployment not proceed"]);
+        }
+
         $event = new GithubDeploymentEvent($pullRequest, $this->githubDeploymentService);
         $this->eventDispatcher->dispatch($event, SurvivalKitEvents::GITHUB_DEPLOYMENT);
 
         try {
             $githubDeploymentService = $event->getDeploymentService();
             $githubDeploymentService->deploy($event->getPullRequest());
-            Logger::notice("[DeploymentController] Github webhook deployment succeeed", LogContext::DEPLOYMENT());
+            Logger::notice("Github webhook deployment succeeed", LogContext::DEPLOYMENT());
+            $response = new JsonResponse("", Response::HTTP_NO_CONTENT);
         } catch (\Throwable $e) {
-            Logger::exception("[DeploymentController] Github webhook deployment failed", LogContext::DEPLOYMENT(), $e);
-
-            return $this->json(["message" => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+            Logger::exception("Github webhook deployment failed", LogContext::DEPLOYMENT(), $e);
+            $response = new JsonResponse(["message" => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
-        return $this->json("", Response::HTTP_NO_CONTENT);
+        return $response;
     }
 
     /**
      * @inheritdoc
      */
-    public function getToken() : string
+    public function getToken(): string
     {
         return $this->getParameter('survival_kit.deployment.secret_token');
     }
